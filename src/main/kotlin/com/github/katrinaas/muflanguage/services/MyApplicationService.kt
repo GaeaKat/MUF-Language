b99c3abe0f5f1d48c1da726a65654e7129810992
GitHub Action <action@github.com>
2021-08-19 00:07:54 +0000

    Template cleanup

--- src/main/kotlin/com/github/katrinaas/muflanguage/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/katrinaas/muflanguage/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.katrinaas.muflanguage.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.katrinaas.muflanguage.MyBundle
 
 class MyApplicationService {
 
